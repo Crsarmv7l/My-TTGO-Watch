2a85586003f7b36af5a64bb9f234ae116ea1b852
Crsarmv7l <85343771+Crsarmv7l@users.noreply.github.com>
2022-01-29 14:45:29 +0900

    Dirty hack to accommodate Weather messages
    Should keep BLE on for ~20min every hour to receive weather messages. might be able to optimize further

--- src/hardware/blectl.cpp
+++ src/hardware/blectl.cpp
@@ -502,7 +502,17 @@ bool blectl_get_enable_on_standby( void ) {
 }
 
 bool blectl_get_disable_only_disconnected( void ) {
-    return( blectl_config.disable_only_disconnected );
+    time_t now;
+    struct tm info;
+    time( &now );
+    localtime_r( &now, &info );
+    int m = info.tm_min;
+    if( m > 30 && m < 50){
+        return( blectl_config.disable_only_disconnected=true);
+    }
+    else {
+        return( blectl_config.disable_only_disconnected=false);
+    }
 }
 
 bool blectl_get_show_notification( void ) {
