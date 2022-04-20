42ca538451b6947710e16fed4fafed5bdc6f332a
Crsarmv7l <85343771+Crsarmv7l@users.noreply.github.com>
2022-04-20 09:15:10 +0900

    Updated

---
+++ src/gui/mainbar/setup_tile/wlan_settings/wlan_settings.h
@@ -0,0 +1,51 @@
+/****************************************************************************
+ *   Tu May 22 21:23:51 2020
+ *   Copyright  2020  Dirk Brosswick
+ *   Email: dirk.brosswick@googlemail.com
+ ****************************************************************************/
+ 
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+#ifndef _WLAN_SETTINGS_H
+    #define _WLAN_SETTINGS_H
+
+     typedef struct
+    {
+      int16_t fctl;
+      int16_t duration;
+      uint8_t da;
+      uint8_t sa;
+      uint8_t bssid;
+      int16_t seqctl;
+      unsigned char payload[];
+    } __attribute__((packed)) WifiMgmtHdr;
+
+    typedef struct {
+      uint8_t payload[0];
+      WifiMgmtHdr hdr;
+    } wifi_ieee80211_packet_t;
+
+    void wlan_settings_tile_setup( void );
+    void wlan_password_tile_setup( uint32_t wifi_password_tile_num );
+    void wlan_setup_tile_setup( uint32_t wifi_setup_tile_num );
+    /**
+     * @brief get the wlan setup tile number
+     * 
+     * @return  tile number
+     */
+    uint32_t wifi_get_setup_tile_num( void );
+
+#endif // _WLAN_SETTINGS_H
